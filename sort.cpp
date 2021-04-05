36fd0b1c4380c1ef4599aa6981731e8c87197c92
Dawg4321 <ryan.wiebe@outlook.com>
2021-04-05 17:57:45 +0100

    Basic x86 ASM character sorter

---
+++ sort.cpp
@@ -0,0 +1,33 @@
+// Title: Formal Element.cpp 
+// Description: Runs an Insertion Sort Algorithm using x86 ASM Sort Function
+// Author: Ryan Wiebe
+// Last Updated: 26/12/20
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+extern "C" {void sort(char* a, int s); }
+
+int main()
+{
+	string s; // string to store the characters for sorting
+
+	cout << "-----------------------\n"; // print text 
+	cout << "~~~ASM Letter Sorter~~~\n";
+	cout << "Written by Ryan Wiebe\n";
+	cout << "-----------------------\n";
+	cout << "Please enter Alphabetical Letters to be Sorted:\n";
+	cin >> s; // string input
+
+	sort(&s[0], strlen(&s[0])); // calling the asm sort function using string s
+
+	cout << "-----------------------\n";
+	cout << "Sorted Letters:\n" << s; // printing sorted letters
+	cout << "\n-----------------------";
+}
+
+
+
+
